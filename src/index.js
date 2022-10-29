12e4666d4fe471bab3e6db0d6c91cca367dd29f2
TLAMHutto <keatonhutto8@gmail.com>
2022-10-29 04:30:49 -0500

    earth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React, { Suspense } from "react";
+import React, { useRef, } from "react";
 import { createRoot } from 'react-dom/client'
-import { Canvas, useThree, useLoader } from '@react-three/fiber'
+import { Canvas, useThree, useLoader, useFrame } from '@react-three/fiber'
 import { OrbitControls, Stars } from "@react-three/drei";
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import * as THREE from "three";
@@ -15,7 +15,7 @@ import "./styles.css";
 const Scene = () => {
   useThree(({camera}) => {
     camera.position.set(0, 10, 0);
-    
+  
     
   });
   
@@ -24,22 +24,26 @@ const Scene = () => {
 const Sphere = () => {
   const color = useLoader(THREE.TextureLoader, map);
   const displace = useLoader(THREE.TextureLoader, height);
-
+  const mesh = useRef()
+  useFrame((state, delta) => (mesh.current.rotation.y += 0.001))
   return (
-    <mesh>
-      <sphereBufferGeometry args={[5, 24, 24]} />
+    <mesh ref={mesh} rotation-x={Math.PI * 0.25} rotation-y={Math.PI * 0.25}>
+      <sphereBufferGeometry args={[5, 100, 100]} />
       <meshStandardMaterial attach="material" color="white" metalness={0.2} map={color} normalMap={displace}/>
     </mesh>
+    
   );
+  
 };
+
 createRoot(document.getElementById('root')).render(
 	 <Canvas >
     
     <Scene />
 		<OrbitControls />
 		<Stars />
-		<ambientLight intensity={0.5} /> 
-		<spotLight position={[10, 15, 10]} angle={0.3} />
+		<ambientLight intensity={0.05} /> 
+		<spotLight position={[15, 15, -10]} angle={0.3} />
 			<Sphere />
 	</Canvas> 
 );
