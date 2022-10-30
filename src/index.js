2b01207c85945b97d168b06ede9907d9b922825c
TLAMHutto <keatonhutto8@gmail.com>
2022-10-29 23:44:53 -0500

    sun

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React, { useRef, } from "react";
 import { createRoot } from 'react-dom/client'
 import { Canvas, useThree, useLoader, useFrame } from '@react-three/fiber'
 import { OrbitControls, Stars } from "@react-three/drei";
+// import { CubeTextureLoader } from "three";
 // import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import * as THREE from "three";
 import map from "./assets/8k_earth_daymap.png";
 import height from "./assets/earth_normals_lowres.png";
+// import nx from './assets/nx.png'
+// import ny from './assets/ny.png'
+// import nz from './assets/nz.png'
+// import px from './assets/px.png'
+// import py from './assets/py.png'
+// import pz from './assets/pz.png'
 // import normal from "./assets/elev.png";
 import "./styles.css";
 
@@ -23,11 +30,11 @@ const Starz = () => {
   const stars = useRef()
   useFrame(({ clock }) => {
     const a = clock.getElapsedTime()
-    stars.current.position.x = a*2
-    stars.current.position.z = a*2
+    stars.current.rotation.x = a*0.05
+    stars.current.rotation.z = a*0.05
   })
   return (
-    <Stars ref={stars} radius={100} depth={50} count={5000} factor={4} saturation={10} fade />
+    <Stars ref={stars} radius={10} depth={500} count={5000} factor={9} saturation={10} fade />
   )
 }
 
@@ -61,30 +68,35 @@ const Moon = () => {
   )
 }
 const SpotLight = () => {
-  const light = useRef()
-  useFrame((state, delta) => (light.current.rotation.y += 1))
+  const spotLight = useRef()
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime()
+    spotLight.current.position.x = Math.cos(a*0.1) * 400
+    spotLight.current.position.z = Math.sin(a*0.1) * 400
+  })
   return (
-    <spotLight ref={light} intensity={2} position={[300, 300, 4000]} angle={0.90} penumbra={1} />
+    <pointLight castShadow ref={spotLight} intensity={2} position={[10, 10, 10]} />
   );
 }
-
-const Axis = () => {
-  return (
-    <mesh>
-      <axesHelper args={[100]} />
-    </mesh>
-  );
-}
+// function SkyBox () {
+//   const {scene} = useThree()
+//   const loader = new CubeTextureLoader()
+//   const texture = loader.load([
+//     px,nx,py,ny,pz,nz
+//   ])
+//   scene.background = texture
+//   return null
+// }
 
 createRoot(document.getElementById('root')).render(
-	 <Canvas >
-    <Axis />
+	 <Canvas shadowMap>
+    {/* <SkyBox /> */}
     <Scene/>
 		<OrbitControls />
 		<Starz />
 		<ambientLight intensity={0.05} /> 
 		<SpotLight />
-			<Sphere />
-      <Moon position={[10,10,10]}/>
+			<Sphere castShadow/>
+      <Moon receiveShadow position={[10,10,10]}/>
 	</Canvas> 
 );
